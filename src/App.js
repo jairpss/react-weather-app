be709989511f550211b32900cab964fc78f7ce03
Jair Partida <jairpartida1@gmail.com>
2022-08-01 22:22:07 -0600

    adding the packages needed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Page from './components/Page';
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App </h1>
+      <Page/>
     </div>
   );
 }
