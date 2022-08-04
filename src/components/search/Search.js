1c2bd2444a540c68da3b519f63b27bdaeb409dcc
Jair Partida <jairpartida1@gmail.com>
2022-08-03 22:08:05 -0600

    adding GEOAPI

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,22 +1,34 @@
 import { useState } from 'react'
 import { AsyncPaginate } from 'react-select-async-paginate'
+import {GEO_API_URL, geoApiOptions} from '../../api'
 
 const Search = ({onSearchChange}) => {
 
-  const [search, setSearch] = useState(null)
+    const [search, setSearch] = useState(null)
 
-  const handleOnChange = (searchData) => {
-    setSearch(searchData)
-    onSearchChange(searchData)
-  }
-  return (
-    <AsyncPaginate 
-        placeholder="Search for city"
-        debounceTimeout={600}
-        value={search}
-        onChange={handleOnChange}
-    />
-  )
+    const loadOptions = (inputValue) => {
+        return fetch(
+            `${GEO_API_URL}/cities?minPopulation=500000&namePrefix={inputValue`, 
+            geoApiOptions
+        )
+            .then(response => response.json())
+            .then(response => console.log(response))
+            .catch(err => console.error(err));
+    }
+
+    const handleOnChange = (searchData) => {
+        setSearch(searchData)
+        onSearchChange(searchData)
+    }
+    return (
+        <AsyncPaginate 
+            placeholder="Search for city"
+            debounceTimeout={600}
+            value={search}
+            onChange={handleOnChange}
+            loadOptions={loadOptions}
+        />
+    )
 }
 
 export default Search
